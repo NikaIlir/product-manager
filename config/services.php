8eabc2d43427d7df8add4abcb135e64f60cca67d
Ilir <ilirnk@gmail.com>
2024-01-14 02:12:16 +0100

    Add migrations and configurations

--- config/services.php
+++ config/services.php
@@ -31,4 +31,8 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'fake_store' => [
+        'api_url' => env('FAKE_STORE_API_URL', 'https://fakestoreapi.com/products'),
+    ]
+
 ];
---
+++ database/migrations/2024_01_08_204222_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', static function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('external_id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('category');
+            $table->string('image');
+            $table->decimal('price', 8, 2);
+            $table->decimal('rating_rate', 2, 1)->default(0);
+            $table->integer('rating_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
